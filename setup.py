26efdfdfc17cbf391368c45b6dbb49ad4d3d8a78
kojix2 <2xijok@gmail.com>
2021-06-20 07:37:53 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 from setuptools import find_packages, setup
 
-
-def read(path):
-    with open(path, "r") as f:
-        return f.read()
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="seaborn-command",
     version="0.0.1",
     description="seaborn command line tool",
-    long_description=read("README.md"),
+    long_description=long_description,
     author="kojix2",
     author_email="2xijok@gmail.com",
-    install_requires=read("requirements.txt").strip().split("\n"),
+    install_requires=["seaborn", "pandas"],
     entry_points={"console_scripts": ["seaborn=seaborn_command.main:main"],},
     url="https://github.com/kojix2/seaborn-command",
-    license=read("LICENSE"),
+    license='MIT',
     packages=find_packages(exclude=("tests", "docs")),
 )
