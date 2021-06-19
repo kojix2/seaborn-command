a604bc8da5a72e5c5f9cd2526381cee7a529e7a9
kojix2 <2xijok@gmail.com>
2021-06-19 19:25:19 +0900

    Improve main

--- sns/main.py
+++ sns/main.py
@@ -1,5 +1,16 @@
+import argparse
+import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 def main():
-    print("hello world")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--delimiter", help="delimiter", default='\t')
+    parser.add_argument("-x", help="X")
+    parser.add_argument("-y", help="Y")
+    args = parser.parse_args()
+    
+    df = pd.read_csv(sys.stdin, sep='\t')
+    sns.pairplot(df, hue="species")
+    plt.savefig(sys.stdout.buffer, format="svg")
