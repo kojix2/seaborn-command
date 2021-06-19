668a2b7078b60df89cf0ff94aa28e187f93ff61e
kojix2 <2xijok@gmail.com>
2021-06-19 19:56:52 +0900

    Improved main.py

--- sns/main.py
+++ sns/main.py
@@ -9,8 +9,13 @@ def main():
     parser.add_argument("-d", "--delimiter", help="delimiter", default='\t')
     parser.add_argument("-x", help="X")
     parser.add_argument("-y", help="Y")
+    parser.add_argument("--format", help="outout image file format")
+    parser.add_argument("--show", help="call plt.show()", action='store_true')
     args = parser.parse_args()
     
-    df = pd.read_csv(sys.stdin, sep='\t')
+    df = pd.read_csv(sys.stdin, sep=args.delimiter)
     sns.pairplot(df, hue="species")
-    plt.savefig(sys.stdout.buffer, format="svg")
+    if args.show:
+      plt.show()
+    else:
+      plt.savefig(sys.stdout.buffer, format=args.format)
