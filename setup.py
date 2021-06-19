02e70ac9d4981a50dcca15695fe20ff1ca9d5bf9
kojix2 <2xijok@gmail.com>
2021-06-19 17:29:14 +0900

    Add version number

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
+def get_version() -> str:
+    version_filepath = os.path.join(ROOT_DIR, PACKAGE_DIRNAME, 'version.py')
+    with open(version_filepath) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return line.strip().split()[-1][1:-1]
+    assert False
+
 setup(
     name='sns',
-    version='0.0.1',
+    version='get_version(),
     description='seaborn command line tool',
     long_description=readme,
     author='kojix2',
