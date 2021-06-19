6491e85f0f410076a55c1cedb899bcbcd0d6cb37
kojix2 <2xijok@gmail.com>
2021-06-19 20:54:16 +0900

    improved main.py

--- sns/main.py
+++ sns/main.py
@@ -6,15 +6,23 @@ import seaborn as sns
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("plot", help="subcommand")
     parser.add_argument("-d", "--delimiter", help="delimiter", default='\t')
-    parser.add_argument("-x", help="X")
-    parser.add_argument("-y", help="Y")
     parser.add_argument("--format", help="outout image file format")
     parser.add_argument("--show", help="call plt.show()", action='store_true')
-    args = parser.parse_args()
-    
+    args, unknown_args = parser.parse_known_args()
+
+    seaborn_args = {}
+    for arg in unknown_args:
+        if arg.startswith('--'):
+            opt = arg[2:]
+        else:
+            seaborn_args[opt] = arg
+
+    print(seaborn_args)
     df = pd.read_csv(sys.stdin, sep=args.delimiter)
-    sns.pairplot(df, hue="species")
+    getattr(sns, args.plot)(df, **seaborn_args)
+
     if args.show:
       plt.show()
     else:
