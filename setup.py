21a2eece12952ba13733eb1077bc6980509a8ea3
kojix2 <2xijok@gmail.com>
2021-06-20 13:41:45 +0900

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="seaborn-command",
-    version="0.0.3",
+    version="0.0.4",
     description="seaborn command line tool",
     long_description=long_description,
     long_description_content_type='text/markdown',
